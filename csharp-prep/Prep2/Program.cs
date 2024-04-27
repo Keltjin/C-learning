b55325b01f87a6fd50e3f088970c20298b84ee83
Keltjin <davigjenn@gmail.com>
2024-04-27 10:11:10 -0600

    Finished Prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        int userScore;
+        char userGrade;
+
+        Console.WriteLine("Welcome to your Pass or Fail Program!");
+
+        Console.Write("\nPlease enter in a grade: ");
+        userScore = int.Parse(Console.ReadLine());
+
+        if (userScore >= 90)
+        {
+            userGrade = 'A';
+        }
+
+        else if (userScore >= 80)
+        {
+            userGrade = 'B';
+        }
+
+        else if (userScore >= 70)
+        {
+            userGrade = 'C';
+        }
+
+        else if (userScore >= 60)
+        {
+            userGrade = 'D';
+        }
+
+        else
+        {
+            userGrade = 'F';
+        }
+
+        Console.WriteLine($"\nYour grade is {userGrade}.");
+
+        if (userScore >= 70)
+        {
+            Console.WriteLine("You have passed!");
+        }
+
+        else
+        {
+            Console.WriteLine("Try again next time.");
+        }
+
     }
 }
